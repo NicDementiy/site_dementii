1b324258a8defb31023e8f7cb1d07fb84ccf4084
NicDementiy <158316539+NicDementiy@users.noreply.github.com>
2024-12-20 10:03:28 +0200

    Add files via upload

---
+++ JS/main2.js
@@ -0,0 +1,11 @@
+/*alert("Привіт, "+prompt("Введіть ваше ім'я","")+"!");
+    alert('Bu...');
+    alert(".....');
+    let isHuman confirm('дина?");
+    alert("Ви...");
+    alert("..лю..');
+    alert("-дина..);
+    9 alert(".....");
+    10
+    13
+    alert(".."+isHuman)//*/
